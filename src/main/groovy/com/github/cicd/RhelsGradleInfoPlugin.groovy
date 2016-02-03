ed6ae943e18c0b941a3e491c386eb89e1f338672
Hari Samala <hari.samala@eyefinity.com>
2016-02-03 12:05:23 -0800

    Extend nebula's gradle-info-plugin

---
+++ src/main/groovy/com/github/cicd/RhelsGradleInfoPlugin.groovy
@@ -0,0 +1,9 @@
+package com.github.cicd
+
+import org.gradle.api.Project
+
+class RhelsGradleInfoPlugin extends nebula.plugin.info.InfoPlugin{
+	void apply(Project project) {
+		super.apply(project)
+	}
+}
